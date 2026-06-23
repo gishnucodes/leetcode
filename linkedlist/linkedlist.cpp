a679f3f161b0c9c217a855b98a5f39d08368e123
gishnucodes <gishnucodes@gmail.com>
2026-06-23 13:06:19 -0400

    linked list

---
+++ linkedlist/linkedlist.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+
+struct ListNode{
+    int data;
+    ListNode* next;
+};
+
+int ListLength(ListNode* head){
+    int length=0;
+    struct ListNode* current=head;
+
+    while(current!=NULL){
+        length++;
+        current=current->next;
+    }
+    return length;
+}
+
+void InsertAtEnd(ListNode** head,int data){
+
+    ListNode* newNode = new ListNode();
+    newNode->data=data;
+    newNode->next=NULL;
+
+    if(head==NULL){
+        return;
+    }
+
+    // what if it is empty
+
+    if(*head==NULL){
+        *head=newNode;
+        return;
+    }
+
+    ListNode* current = *head;
+    while(current->next!=NULL){
+        current = current->next;
+    }
+    current->next = newNode;
+    
+}   
